8b37b651a4d04b568d7350daadf7e65c7607955c
Arush Ramteke <arushramtek@gmail.com>
2022-01-31 10:28:19 -0800

    Removed console.log statements

--- src/components/shared/LessonSide/LevelSelector.tsx
+++ src/components/shared/LessonSide/LevelSelector.tsx
@@ -1,32 +1,29 @@
 import '../../../styles/LessonSide.scss';
 
 interface IProps {
-  items: Number,
-  current: Number,
+  items: number,
+  current: number,
 }
 
 function LevelSelector({items, current}:IProps): JSX.Element {
-  console.log(items);
-  console.log(current);
-
   const levelList = () => {
-    let list = [];
+    const list = [];
     for (let item = 1; item <= items; item++) {
-      let styleClasses = "level-button other-level";
-      if (item == current) {styleClasses = "level-button current-level";}
+      let styleClasses = 'level-button other-level';
+      if (item == current) {styleClasses = 'level-button current-level';}
       list.push(<div className={styleClasses}>{item}</div>);
       if (item != items) {list.push(<div className="level-connector"></div>);}
     }
     return list;
-  }
+  };
 
   return (
     <div className="selector-box">
       <div className="level-selector">
         {levelList()}
-      </div>  
+      </div>
     </div>
-  )
+  );
 }
 
 export default LevelSelector;
