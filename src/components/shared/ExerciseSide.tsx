e9d18209f2304b91c82a6d4e0c611de042fa6c45
Abishan Sutharshan <abishan.sutharshan@gmail.com>
2022-01-31 23:16:28 -0500

    exerciseSide commit test

--- src/components/shared/ExerciseSide.tsx
+++ src/components/shared/ExerciseSide.tsx
@@ -1,7 +1,13 @@
 import '../../styles/ExerciseSide.scss';
 
 function ExerciseSide(): JSX.Element {
-  return <section id="exercise-side-container"></section>;
+  
+  return (<section id="exercise-side-container">
+    <h1> Type the correct numbers into the blanks below! </h1>
+  
+
+  </section>
+  );
 }
 
 export default ExerciseSide;
