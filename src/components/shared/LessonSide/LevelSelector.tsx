2edb38e167735e056fec4dfd66dd0bbf5cb34c27
Arush Ramteke <arushramtek@gmail.com>
2022-01-31 23:42:10 -0800

    Previous commit introduced new changes

--- src/components/shared/LessonSide/LevelSelector.tsx
+++ src/components/shared/LessonSide/LevelSelector.tsx
@@ -1,27 +1,29 @@
 import '../../../styles/LessonSide.scss';
 
 interface IProps {
-  items: number,
-  current: number,
+  items: number;
+  current: number;
 }
 
-function LevelSelector({items, current}:IProps): JSX.Element {
+function LevelSelector({ items, current }: IProps): JSX.Element {
   const levelList = () => {
     const list = [];
     for (let item = 1; item <= items; item++) {
       let styleClasses = 'level-button other-level';
-      if (item == current) {styleClasses = 'level-button current-level';}
+      if (item == current) {
+        styleClasses = 'level-button current-level';
+      }
       list.push(<div className={styleClasses}>{item}</div>);
-      if (item != items) {list.push(<div className="level-connector"></div>);}
+      if (item != items) {
+        list.push(<div className="level-connector"></div>);
+      }
     }
     return list;
   };
 
   return (
     <div className="selector-box">
-      <div className="level-selector">
-        {levelList()}
-      </div>
+      <div className="level-selector">{levelList()}</div>
     </div>
   );
 }
