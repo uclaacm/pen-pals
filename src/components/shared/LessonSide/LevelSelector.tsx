aba459f91880c9ee2f1cd724e5de6284db3b7011
Arush Ramteke <arushramtek@gmail.com>
2022-02-03 01:09:02 -0800

    Lint changes

--- src/components/shared/LessonSide/LevelSelector.tsx
+++ src/components/shared/LessonSide/LevelSelector.tsx
@@ -5,18 +5,31 @@ interface LevelSelectorProps {
   currentLevel: number;
 }
 
-function LevelSelector({ numLevels, currentLevel }: LevelSelectorProps): JSX.Element {
+function LevelSelector({
+  numLevels,
+  currentLevel,
+}: LevelSelectorProps): JSX.Element {
   return (
     <div className="selector-box">
       <div className="level-selector">
         {Array(numLevels * 2 - 1)
           .fill(0)
           .map((_, i) => {
-            const level = i/2 + 1;
+            const level = i / 2 + 1;
             return i % 2 == 1 ? (
-              <div className={"level-connector" + (level < currentLevel ? ' reached-level-connector' : '')}></div>
+              <div
+                className={
+                  'level-connector' +
+                  (level < currentLevel ? ' reached-level-connector' : '')
+                }
+              ></div>
             ) : (
-              <div className={"level-button" + (level == currentLevel ? ' current-level' : '')}>
+              <div
+                className={
+                  'level-button' +
+                  (level == currentLevel ? ' current-level' : '')
+                }
+              >
                 {level}
               </div>
             );
