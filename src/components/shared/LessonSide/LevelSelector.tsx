bcd050bbcebcca39f49fa0b019b7c39c90639792
Arush Ramteke <arushramtek@gmail.com>
2022-02-04 01:10:38 -0800

    Lint changes

--- src/components/shared/LessonSide/LevelSelector.tsx
+++ src/components/shared/LessonSide/LevelSelector.tsx
@@ -3,7 +3,7 @@ import '../../../styles/LessonSide.scss';
 interface LevelSelectorProps {
   numLevels: number;
   currentLevel: number;
-  maxLevelReached: number,
+  maxLevelReached: number;
 }
 
 function LevelSelector({
@@ -15,30 +15,31 @@ function LevelSelector({
     <div className="selector-box">
       <div className="level-selector">
         {
-        //numLevels * 2 - 1 elements in Array as one button for each layer and numLevels - 1 connectors.
-        Array(numLevels * 2 - 1)
-          .fill(0)
-          .map((_, i) => {
-            const level = i / 2 + 1;
-            //If index is even, add a level button. Else, add a level connector.
-            return i % 2 == 0 ? (
-              <div
-                className={
-                  'level-button' +
-                  (level === currentLevel ? ' current-level' : '')
-                }
-              >
-                {level}
-              </div>
-            ) : (
-              <div
-                className={
-                  'level-connector' +
-                  (level < maxLevelReached ? ' reached-level-connector' : '')
-                }
-              ></div>
-            );
-          })}
+          //numLevels * 2 - 1 elements in Array as one button for each layer and numLevels - 1 connectors.
+          Array(numLevels * 2 - 1)
+            .fill(0)
+            .map((_, i) => {
+              const level = i / 2 + 1;
+              //If index is even, add a level button. Else, add a level connector.
+              return i % 2 == 0 ? (
+                <div
+                  className={
+                    'level-button' +
+                    (level === currentLevel ? ' current-level' : '')
+                  }
+                >
+                  {level}
+                </div>
+              ) : (
+                <div
+                  className={
+                    'level-connector' +
+                    (level < maxLevelReached ? ' reached-level-connector' : '')
+                  }
+                ></div>
+              );
+            })
+        }
       </div>
     </div>
   );
