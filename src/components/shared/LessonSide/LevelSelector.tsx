bbd467ea8e92d0c9753262d719e7a7311c7fb52c
Arush Ramteke <arushramtek@gmail.com>
2022-02-04 01:10:13 -0800

    added maxLevelReached props, changed == to ===, added comments

--- src/components/shared/LessonSide/LevelSelector.tsx
+++ src/components/shared/LessonSide/LevelSelector.tsx
@@ -3,35 +3,40 @@ import '../../../styles/LessonSide.scss';
 interface LevelSelectorProps {
   numLevels: number;
   currentLevel: number;
+  maxLevelReached: number,
 }
 
 function LevelSelector({
   numLevels,
   currentLevel,
+  maxLevelReached,
 }: LevelSelectorProps): JSX.Element {
   return (
     <div className="selector-box">
       <div className="level-selector">
-        {Array(numLevels * 2 - 1)
+        {
+        //numLevels * 2 - 1 elements in Array as one button for each layer and numLevels - 1 connectors.
+        Array(numLevels * 2 - 1)
           .fill(0)
           .map((_, i) => {
             const level = i / 2 + 1;
-            return i % 2 == 1 ? (
-              <div
-                className={
-                  'level-connector' +
-                  (level < currentLevel ? ' reached-level-connector' : '')
-                }
-              ></div>
-            ) : (
+            //If index is even, add a level button. Else, add a level connector.
+            return i % 2 == 0 ? (
               <div
                 className={
                   'level-button' +
-                  (level == currentLevel ? ' current-level' : '')
+                  (level === currentLevel ? ' current-level' : '')
                 }
               >
                 {level}
               </div>
+            ) : (
+              <div
+                className={
+                  'level-connector' +
+                  (level < maxLevelReached ? ' reached-level-connector' : '')
+                }
+              ></div>
             );
           })}
       </div>
--- src/components/shared/LessonSide/index.tsx
+++ src/components/shared/LessonSide/index.tsx
@@ -6,7 +6,7 @@ function LessonSide(): JSX.Element {
     <section id="lesson-side-container">
       some stuff
       {/* don't change this next line */}
-      <LevelSelector numLevels={5} currentLevel={2} />
+      <LevelSelector numLevels={5} currentLevel={2} maxLevelReached={4} />
     </section>
   );
 }
