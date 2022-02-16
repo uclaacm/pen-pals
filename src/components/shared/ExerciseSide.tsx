d1dbfe94309d4f3aae65dead19f42deb320858d0
Arush Ramteke <arushramtek@gmail.com>
2022-02-16 00:52:33 -0800

    lint

--- src/components/shared/ExerciseSide.tsx
+++ src/components/shared/ExerciseSide.tsx
@@ -1,12 +1,15 @@
 import '../../styles/ExerciseSide.scss';
-import AxisExercise from './Exercises/AxisExercise';'./Exercises/AxisExercise'
+import AxisExercise from './Exercises/AxisExercise';
+('./Exercises/AxisExercise');
 
 function ExerciseSide(): JSX.Element {
-  return (<section id="exercise-side-container">
-    <div className="exercise-box">
-      <AxisExercise  orientation="horizontal" />
-    </div>
-  </section>);
+  return (
+    <section id="exercise-side-container">
+      <div className="exercise-box">
+        <AxisExercise orientation="horizontal" />
+      </div>
+    </section>
+  );
 }
 
 export default ExerciseSide;
--- src/components/shared/Exercises/AxisExercise.tsx
+++ src/components/shared/Exercises/AxisExercise.tsx
@@ -1,21 +1,21 @@
 import '../../../styles/Exercises/AxisExercise.scss';
-import Axis from '../../../assets/Exercises/axis.svg'
+import Axis from '../../../assets/Exercises/axis.svg';
 
 interface AxisExerciseProps {
-    orientation: string;
+  orientation: string;
 }
 
-function AxisExercise({
-    orientation,
-  }: AxisExerciseProps): JSX.Element {
-
-    return(
-        <div className="axis-container" style={{
-                transform: `rotate(${(orientation === 'horizontal' ? '0deg' : '90deg')})`
-        }}>
-            <img className="axis-img" src={Axis} alt="single-axis"/>
-        </div>
-    )
+function AxisExercise({ orientation }: AxisExerciseProps): JSX.Element {
+  return (
+    <div
+      className="axis-container"
+      style={{
+        transform: `rotate(${orientation === 'horizontal' ? '0deg' : '90deg'})`,
+      }}
+    >
+      <img className="axis-img" src={Axis} alt="single-axis" />
+    </div>
+  );
 }
 
 export default AxisExercise;
