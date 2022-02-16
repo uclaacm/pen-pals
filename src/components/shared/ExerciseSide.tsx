2ba3de8ecaef041e3176f16d469a1a963aaa64b9
Arush Ramteke <arushramtek@gmail.com>
2022-02-16 00:52:18 -0800

    basic axis

--- src/components/shared/ExerciseSide.tsx
+++ src/components/shared/ExerciseSide.tsx
@@ -1,7 +1,12 @@
 import '../../styles/ExerciseSide.scss';
+import AxisExercise from './Exercises/AxisExercise';'./Exercises/AxisExercise'
 
 function ExerciseSide(): JSX.Element {
-  return <section id="exercise-side-container"></section>;
+  return (<section id="exercise-side-container">
+    <div className="exercise-box">
+      <AxisExercise  orientation="horizontal" />
+    </div>
+  </section>);
 }
 
 export default ExerciseSide;
