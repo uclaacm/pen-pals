9608b3de0de7981ee951f244ea3a92800d10bcc7
Abishan Sutharshan <abishan.sutharshan@gmail.com>
2022-02-25 16:30:58 -0800

    New Push 2/25/22

--- src/components/shared/ExerciseSide.tsx
+++ src/components/shared/ExerciseSide.tsx
@@ -1,13 +1,22 @@
 import '../../styles/ExerciseSide.scss';
 
 function ExerciseSide(): JSX.Element {
-  
-  return (<section id="exercise-side-container">
-    <h1> Type the correct numbers into the blanks below! </h1>
-  
+  return (
+    <section id = "exercise-side-container">
+      <div id = "level">
+      
+      <h1> Type the correct numbers into the blanks below! </h1>
+      <h1> A = (2,0) </h1>
+      <h1> B = (-2,0) </h1>
+      <h1> C = (-1,0) </h1>
 
-  </section>
-  );
+     </div>
+     
+     <button id = "button"> Check </button>
+
+    </section>
+  )
 }
+
 
 export default ExerciseSide;
