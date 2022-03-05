52383d1e0aa054a56b717345b60e6455ab19956b
Arush Ramteke <arushramtek@gmail.com>
2022-03-04 16:15:14 -0800

    lint

--- src/components/shared/ExerciseSide.tsx
+++ src/components/shared/ExerciseSide.tsx
@@ -6,7 +6,11 @@ function ExerciseSide(): JSX.Element {
   return (
     <section id="exercise-side-container">
       <div className="exercise-box">
-        <AxisExercise orientation="horizontal" markers={[-2, -1, 0, 1, 2]} labels={['A', '', '', 'B', 'C']}/>
+        <AxisExercise
+          orientation="horizontal"
+          markers={[-2, -1, 0, 1, 2]}
+          labels={['A', '', '', 'B', 'C']}
+        />
       </div>
     </section>
   );
--- src/components/shared/Exercises/AxisExercise.tsx
+++ src/components/shared/Exercises/AxisExercise.tsx
@@ -7,42 +7,57 @@ interface AxisExerciseProps {
   labels: string[];
 }
 
-function AxisExercise({ orientation, markers, labels }: AxisExerciseProps): JSX.Element {
-  const markerSpacing = 60/(markers.length - 1);
+function AxisExercise({
+  orientation,
+  markers,
+  labels,
+}: AxisExerciseProps): JSX.Element {
+  const markerSpacing = 60 / (markers.length - 1);
   return (
-    <div className="axis-container" style={{
-      transform: `rotate(${orientation === 'horizontal' ? '0deg' : '90deg'})`,
-    }}>
+    <div
+      className="axis-container"
+      style={{
+        transform: `rotate(${orientation === 'horizontal' ? '0deg' : '90deg'})`,
+      }}
+    >
       <img className="axis-img" src={Axis} alt="single-axis" />
-      {
-        markers.map((pos, idx) => {
-          const leftPosition = 45 + (markerSpacing * pos);
-          return (
+      {markers.map((pos, idx) => {
+        const leftPosition = 45 + markerSpacing * pos;
+        return (
+          <div
+            className="marker-container"
+            style={{ left: `${leftPosition}%` }}
+            key={pos}
+          >
             <div
-              className="marker-container" 
-              style={{left: `${leftPosition}%`}}
-              key={pos}
+              className="marker-label"
+              style={{
+                transform: `rotate(${
+                  orientation === 'horizontal' ? '0deg' : '270deg'
+                })`,
+              }}
             >
-              <div className = "marker-label" style={{
-                transform: `rotate(${orientation === 'horizontal' ? '0deg' : '270deg'})`
-              }}>
-                {labels[idx]}
-              </div>
-              <div
-                className = "marker"
-                style = {{
-                  width: `calc(${(pos == 0 ? 20 : 8)}px + 1vw)`
-                }}
-              />
-              <div className="marker-pos" style={{
-                transform: `rotate(${orientation === 'horizontal' ? '0deg' : '270deg'})`
-              }}>
-                {pos}
-              </div>
+              {labels[idx]}
             </div>
-          );
-        })
-      }
+            <div
+              className="marker"
+              style={{
+                width: `calc(${pos == 0 ? 20 : 8}px + 1vw)`,
+              }}
+            />
+            <div
+              className="marker-pos"
+              style={{
+                transform: `rotate(${
+                  orientation === 'horizontal' ? '0deg' : '270deg'
+                })`,
+              }}
+            >
+              {pos}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
