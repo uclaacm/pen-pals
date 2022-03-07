0636f0662e9a50ac60baf7ce019d37328c37deac
Lydia Chan <chan.lydia528@gmail.com>
2022-03-06 22:54:30 -0800

    Add in Unit Circle with user-inputted tick values and props

--- src/components/shared/ExerciseSide.tsx
+++ src/components/shared/ExerciseSide.tsx
@@ -1,7 +1,20 @@
 import '../../styles/ExerciseSide.scss';
+import {UnitCircleTick, UnitCircleExercise} from './UnitCircleExercise';
+
+let labelOne: UnitCircleTick =({label: 'A', type: "unit-circle-unknown-label"});
+let labelTwo: UnitCircleTick =({label: 'C', type: "unit-circle-unknown-label"});
+let labelThree: UnitCircleTick =({label: '0˚', type: "unit-circle-known-label"});
+let labelFour: UnitCircleTick =({label: 'B', type: "unit-circle-unknown-label"});
+let labelFive: UnitCircleTick =({label: '90˚', type: "unit-circle-known-label"});
 
 function ExerciseSide(): JSX.Element {
-  return <section id="exercise-side-container"></section>;
+  return (
+    <section id="exercise-side-container">
+
+      <UnitCircleExercise turtleAngle={10} labelOne={labelOne} labelTwo={labelTwo} labelThree={labelThree} labelFour={labelFour} labelFive={labelFive}/>
+
+    </section>
+  );
 }
 
 export default ExerciseSide;
