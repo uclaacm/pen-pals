7dc03184b404ede7eb3201955676c8df96a2a249
Arush Ramteke <arushramtek@gmail.com>
2022-04-11 16:28:32 -0700

    Fixed turtle centering issue

--- src/components/shared/Exercises/AxisExercise.tsx
+++ src/components/shared/Exercises/AxisExercise.tsx
@@ -16,8 +16,6 @@ function AxisExercise({
   turtlePosition,
 }: AxisExerciseProps): JSX.Element {
   const markerSpacing = 60 / (markers.length - 1);
-  // calculate turtle position: add 1 so centered with respect to shell and not whole image
-  const turtleLeftPos = 45 + markerSpacing * turtlePosition;
   return (
     <div
       className="axis-container"
@@ -25,22 +23,24 @@ function AxisExercise({
         transform: `rotate(${orientation === 'horizontal' ? '0deg' : '90deg'})`, // axis orientation
       }}
     >
-      <img
-        className="turtle-img"
-        src={Turtle}
-        alt="turtle-icon"
-        style={{ left: `${turtleLeftPos}%` }}
-      />
       <img className="axis-img" src={Axis} alt="single-axis" />
       {markers.map((pos, idx) => {
         // map markers and corresponding labels onto axis
-        const leftPosition = 45 + markerSpacing * pos; // calculate posiition for marker
+        const leftPosition = 45 + markerSpacing * pos; // calculate position for marker
         return (
           <div
             className="marker-container"
             style={{ left: `${leftPosition}%` }}
             key={pos}
           >
+            {pos === turtlePosition ? 
+                <img
+                  className="turtle-img"
+                  src={Turtle}
+                  alt="turtle-icon"
+                />
+              : null
+            }
             <div // marker label (A, B, C, ...)
               className="marker-label"
               style={{
