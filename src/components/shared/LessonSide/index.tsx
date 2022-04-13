ddd3cb9bf24e96d8e7163c082c13385e37280f71
jpaten <jcpaten@gmail.com>
2022-04-12 18:54:30 -0700

    Basic Lesson Side Implementation (#16)
    * Created bad but existent mockup for lesson side
    
    * Split up some long lines to stop complaints
    
    * Improved responsive design elements
    
    * Fixing up font issues and merge conflicts
    
    * Fixed a few more issues related to the merge
    
    * Made changes to the fonts and addressed linting issues(and actually added them)
    
    * Committing something random before merging
    
    * Tried to fix issue where lesson selector got stuck on bottom
    
    * Stopped level selector getting lost in footer and actually staged changes
    
    * Part of attempt to fix spacing
    
    * Finally moved things to the bottom correctly
    
    * Made sure footer works'
    
    * Addressed code quality issues as requested
    
    * Linting fix
    
    * Slightly increased font size, used variables better
    
    * fixed linting issues
    
    Co-authored-by: 8BitRobot <premgiridhar11@gmail.com>

--- src/components/shared/LessonSide/index.tsx
+++ src/components/shared/LessonSide/index.tsx
@@ -1,12 +1,48 @@
 import '../../../styles/LessonSide.scss';
+import encouragingTurtle from '../../../assets/EncouragingTurtle.svg'; // <3 encouraging turtle
+import penPalsLogo from '../../../assets/turtleLogo.svg';
 import LevelSelector from './LevelSelector';
 
 function LessonSide(): JSX.Element {
   return (
     <section id="lesson-side-container">
-      some stuff
-      {/* don't change this next line */}
-      <LevelSelector numLevels={5} currentLevel={2} maxLevelReached={4} />
+      <div>
+        <div id="turtle-logo">
+          <img
+            src={penPalsLogo}
+            alt="Small turtle, part of the TeachLA logo"
+            id="turtle-image"
+          />
+          <div id="turtle-logo-text">Pen Pals</div>
+        </div>
+        <div id="lesson-side-header">Positioning in Computers</div>
+        <div id="lesson-side-body">
+          In order to tell computers where to move the cursor, we have to use
+          the coordinate system. The coordinate system is a grid with lots of
+          small chunks, called units. To figure out where to move, the cursor
+          uses its starting point at the center of the grid and counts out some
+          number of units. For example, the point (2, 1) would be two units
+          RIGHT and one unit UP from the center.
+        </div>
+        <div id="lesson-side-body">
+          When looking at these numbers, remember that the number to move
+          sideways comes first. A positive number means you move right, and a
+          negative number means you move left. And when moving up and down with
+          the second number, a positive number means you move up and a negative
+          means down.
+        </div>
+      </div>
+      <div>
+        <img
+          src={encouragingTurtle}
+          alt='Turtle saying "You can do it!"'
+          id="encouraging-turtle"
+        />
+
+        {/* don't change this next line */}
+        <LevelSelector numLevels={5} currentLevel={2} maxLevelReached={4} />
+        <div id="footer-space" />
+      </div>
     </section>
   );
 }
