7b5a55e7cbcb4caae579d3d86534e9f3ba110b9d
Jonah Paten <jpaten@ucla.edu>
2022-04-29 17:41:11 -0700

    Redesigned footer

---
+++ src/components/shared/LessonSide/Footer.tsx
@@ -0,0 +1,26 @@
+import TeachLALogo from '../../../assets/teach-la-logo.svg';
+import WhiteHeart from "../../../assets/whiteHeart.svg"
+import '../../../styles/Footer.scss';
+
+function Footer(): JSX.Element {
+  return (
+    <div id="footer">
+      <h3>
+        made with {' '}<img id="tla-logo" src={WhiteHeart} alt="A white heart"/> {'  '}
+        by{' '}
+        <a
+          href="https://teachla.uclaacm.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="footer-link"
+        >
+          {' '}
+          <img id="tla-logo" src={TeachLALogo} alt="teach la logo!" />
+          acm.teachLA
+        </a>
+      </h3>
+    </div>
+  );
+}
+
+export default Footer;
