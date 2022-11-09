878f6526376e861d0192111eaf3ae5b201f28f7b
jennifer2430 <jchen243028@gmail.com>
2022-11-08 18:52:57 -0800

    Added unit circle exercise to exercise side

--- src/components/shared/ExerciseSide.tsx
+++ src/components/shared/ExerciseSide.tsx
@@ -1,6 +1,7 @@
 import '../../styles/ExerciseSide.scss';
 import AxisExercise from './Exercises/AxisExercise';
 ('./Exercises/AxisExercise');
+import UnitCircleExercise from './Exercises/UnitCircleExercise';
 
 function ExerciseSide(): JSX.Element {
   return (
@@ -11,6 +12,11 @@ function ExerciseSide(): JSX.Element {
           markers={[-2, -1, 0, 1, 2]}
           labels={['A', '', '', 'B', 'C']}
           turtlePosition={1}
+        />
+        <UnitCircleExercise
+          turtleAngle={0}
+          markers={['0', '', '', '', '']}
+          labels={['', 'A', 'B', 'E', 'G']}
         />
       </div>
     </section>
