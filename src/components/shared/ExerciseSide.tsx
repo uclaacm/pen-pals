187787db380416540da37a78ce947d595e3cd0e7
Tom Binford <tjbof123@gmail.com>
2023-01-30 17:17:44 -0800

    Change grid lines to have equal horizontal/vertical spacing. Make Graph component use percentage rather than pixel-based sizing.

--- src/components/shared/ExerciseSide.tsx
+++ src/components/shared/ExerciseSide.tsx
@@ -1,14 +1,22 @@
 import '../../styles/ExerciseSide.scss';
-
 import AxisExercise from './Exercises/AxisExercise';
 import AxisInput from './Exercises/AxisInputs';
+import Graph from './Graph';
 ('./Exercises/AxisExercise');
 
 function ExerciseSide(): JSX.Element {
   return (
     <section id="exercise-side-container">
       <div className="exercise-box">
-        <AxisExercise
+        <Graph
+          origin={{ x: 0, y: 0 }}
+          points={[]}
+          labels={[]}
+          cursorPosition={{ x: 1, y: 2 }}
+          cursorOrientation={0}
+        />
+
+        {/*<AxisExercise
           orientation="horizontal"
           markers={[-2, -1, 0, 1, 2]}
           labels={['A', '', '', 'B', 'C']}
