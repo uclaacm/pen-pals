ecbcfaad02c545c8ffc67d5f5720f39fe3719dee
RoyalAscot1 <66349492+RoyalAscot1@users.noreply.github.com>
2023-02-14 17:26:57 -0800

    changes

--- src/components/shared/Exercises/ExerComplete.tsx
+++ src/components/shared/Exercises/ExerComplete.tsx
@@ -1,23 +1,20 @@
-import { useState } from 'react'; 
 import '../../../styles/Exercises/ExerComplete.scss';
 import encouragingTurtle from '../../../assets/EncouragingTurtle-Solo.svg';
 
 interface ExerCompleteInputs {
-    exerciseName: string
+  exerciseName: string;
 }
 
-function ExerComplete({exerciseName}: ExerCompleteInputs):
-    JSX.Element {
-        return (
-            <div className="exercomplete-container">
-                <h2>Congratulations!</h2>
-                <p>You have completed the {exerciseName} exercise</p>
-                <img src={encouragingTurtle}/>
-                <p></p>
-                <button className="exercomplete-continue-button">Continue</button>
-            </div>
-        );
+function ExerComplete({ exerciseName }: ExerCompleteInputs): JSX.Element {
+  return (
+    <div className="exercomplete-container">
+      <h2>Congratulations!</h2>
+      <p>You have completed the {exerciseName} exercise</p>
+      <img src={encouragingTurtle} />
+      <p></p>
+      <button className="exercomplete-continue-button">Continue</button>
+    </div>
+  );
 }
 
-export default ExerComplete
-    
+export default ExerComplete;
