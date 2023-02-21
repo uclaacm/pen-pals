35d74cc691a80edd4b63bd31e2afaf87cd7c21ae
RoyalAscot1 <66349492+RoyalAscot1@users.noreply.github.com>
2023-02-21 11:18:06 -0800

    added function input to ExerciseComplete

--- src/components/shared/Exercises/ExerciseComplete.tsx
+++ src/components/shared/Exercises/ExerciseComplete.tsx
@@ -6,14 +6,19 @@ interface ExerciseCompleteInputs {
   onComplete: () => void;
 }
 
-function ExerciseComplete({ exerciseName, onComplete }: ExerciseCompleteInputs): JSX.Element {
+function ExerciseComplete({
+  exerciseName,
+  onComplete,
+}: ExerciseCompleteInputs): JSX.Element {
   return (
     <div className="exercisecomplete-container">
       <h2>Congratulations!</h2>
       <p>You have completed the {exerciseName} exercise</p>
       <img src={encouragingTurtle} />
       <p></p>
-      <button className="exercisecomplete-continue-button" onClick={onComplete}>Continue</button>
+      <button className="exercisecomplete-continue-button" onClick={onComplete}>
+        Continue
+      </button>
     </div>
   );
 }
