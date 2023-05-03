420ac5d3b0e094eb58f6a936cb9df260695ada4d
Michelle Lee <michellehlee07@gmail.com>
2023-05-02 18:27:49 -0700

    fixed some lint errors

--- src/components/shared/LessonSide/LessonText.tsx
+++ src/components/shared/LessonSide/LessonText.tsx
@@ -1,18 +1,17 @@
 import '../../../styles/LessonSide.scss';
 
 type LessonTextType = {
-    id: string;
-    text: string;
-}
+  id: string;
+  text: string;
+};
 
 interface LessonTextProps {
-    text_array: LessonTextType[];
+  text_array: LessonTextType[];
 }
 
-
-function LessonText({text_array} : LessonTextProps): JSX.Element {
-    const list = text_array.map(body => <div id={body.id}>{body.text}</div>);
-     return<div>{list}</div>;
+function LessonText({ text_array }: LessonTextProps): JSX.Element {
+  const list = text_array.map((body) => <div id={body.id}>{body.text}</div>);
+  return <div>{list}</div>;
 }
 
-export default LessonText
+export default LessonText;
