8645d624ba95c6992cfea99b72e2fb0f952e9979
Jonah Paten <jpaten@ucla.edu>
2023-05-09 13:09:27 -0700

    Created dummy graphinput component #43

--- src/components/shared/Exercises/GraphInput.tsx
+++ src/components/shared/Exercises/GraphInput.tsx
@@ -11,7 +11,10 @@ interface GraphQuestion {
   textArray: GraphQuestionElement[];
 }
 
-interface GraphInputProps {}
+interface GraphInputProps {
+  questionArray: GraphQuestion[];
+  nextExercise: () => void;
+}
 
 function GraphStringElement({
   type,
@@ -20,31 +23,72 @@ function GraphStringElement({
   answer,
 }: GraphQuestionElement) {
   if (type == 'text') {
-    return <p>{text}</p>;
+    return <p id="graphinput-check-question">{text}</p>;
   } else {
-    return <input />;
+    return (
+      <input
+        type="text"
+        id="graphinput-check-box"
+        style={{ width: `${width * 16}px` }}
+      />
+    );
   }
 }
 
 function GraphLine({ textArray }: GraphQuestion) {
   //Map all elements of TextArray
+  const makeElement = (elementData: GraphQuestionElement): JSX.Element => {
+    return (
+      <GraphStringElement
+        type={elementData?.type}
+        text={elementData?.text}
+        width={elementData?.width}
+        answer={elementData?.answer}
+      />
+    );
+  };
   return (
+    <div id={'graphinput-question-container'}>{textArray.map(makeElement)}</div>
+  );
+  /*return (
     <div id="graphinput-question-container">
-      <p id="graphinput-check-question">Hi</p>
-      <input type="text" id="graphinput-check-box" style={{ width: '$3vw' }} />
+      <GraphStringElement type={"text"} text={"Hi?"}/>
+      <GraphStringElement type={"beans"} width={4} text={""}/>
       <p id={'graphinput-check-question'}> Bye</p>
     </div>
-  );
+  );*/
 }
 
-function GraphInput(): JSX.Element {
+function GraphInput({
+  questionArray,
+  nextExercise,
+}: GraphInputProps): JSX.Element {
+  const makeLine = (lineArray: GraphQuestion): JSX.Element => {
+    return <GraphLine textArray={lineArray.textArray} />;
+  };
   return (
     <div id="graphinput-container">
-      <div id="graphinput-question-box">
-        <GraphLine textArray={[]} />
+      <div id="graphinput-question-box">{questionArray.map(makeLine)}</div>
+      <div id="graphinput-check-button-container">
+        <button id="graphinput-check-button" onClick={nextExercise}>
+          Check
+        </button>
       </div>
     </div>
   );
+  /*
+  return (
+    <div id="graphinput-container">
+      <div id="graphinput-question-box">
+        <GraphLine
+          textArray={[
+            { text: 'Hi!', type: 'text' },
+            { type: 'input', width: 10 },
+          ]}
+        />
+      </div>
+    </div>
+  );*/
 }
 
 export default GraphInput;
