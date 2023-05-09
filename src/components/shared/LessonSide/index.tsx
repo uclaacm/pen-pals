f5f1a7f519d10048b6925ceda96e2e40aa621ca0
Michelle Lee <68783320+michellelee0718@users.noreply.github.com>
2023-05-09 14:57:03 -0700

    44 better organize lessonside (#49)
    * created new LessonText component
    
    * fixed some lint errors
    
    * fixed lint issue
    
    * fixed lint issue
    
    * checking for lint errors
    
    * Changed key= to id= so CSS displayed correctly #44
    
    * Added a key using the text of the LessonText entry #44
    
    ---------
    
    Co-authored-by: Jonah Paten <jpaten@ucla.edu>

--- src/components/shared/LessonSide/index.tsx
+++ src/components/shared/LessonSide/index.tsx
@@ -1,8 +1,21 @@
 import '../../../styles/LessonSide.scss';
 import penPalsLogo from '../../../assets/turtleLogo.svg';
 import Footer from './Footer';
+import LessonText from './LessonText';
 import LevelSelector from './LevelSelector';
 import Turtle from './Turtle';
+
+const exArray: { id: string; text: string }[] = [
+  { id: 'lesson-side-header', text: 'Positioning of Computers' },
+  {
+    id: 'lesson-side-body',
+    text: 'In order to tell computers where to move the cursor, we have to use the coordinate system. The coordinate system is a grid with lots of small chunks, called units. To figure out where to move, the cursor uses its starting point at the center of the grid and counts out some number of units. For example, the point (2, 1) would be two units RIGHT and one unit UP from the center.',
+  },
+  {
+    id: 'lesson-side-body',
+    text: 'When looking at these numbers, remember that the number to move sideways comes first. A positive number means you move right, and a negative number means you move left. And when moving up and down with the second number, a positive number means you move up and a negative means down.',
+  },
+];
 
 function LessonSide(): JSX.Element {
   return (
@@ -16,22 +29,7 @@ function LessonSide(): JSX.Element {
           />
           <div id="turtle-logo-text">Pen Pals</div>
         </div>
-        <div id="lesson-side-header">Positioning in Computers</div>
-        <div id="lesson-side-body">
-          In order to tell computers where to move the cursor, we have to use
-          the coordinate system. The coordinate system is a grid with lots of
-          small chunks, called units. To figure out where to move, the cursor
-          uses its starting point at the center of the grid and counts out some
-          number of units. For example, the point (2, 1) would be two units
-          RIGHT and one unit UP from the center.
-        </div>
-        <div id="lesson-side-body">
-          When looking at these numbers, remember that the number to move
-          sideways comes first. A positive number means you move right, and a
-          negative number means you move left. And when moving up and down with
-          the second number, a positive number means you move up and a negative
-          means down.
-        </div>
+        <LessonText text_array={exArray} />
       </div>
       <div>
         <Turtle turtleID="your-genius" />
