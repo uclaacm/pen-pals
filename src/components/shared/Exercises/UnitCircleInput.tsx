f2e70eeddd87ec3d1a7f42d59a46d58f82db312f
Skylar Tanartkit <sltanartkit@g.ucla.edu>
2023-05-16 18:20:50 -0700

    unit circle inpu t mostly done #42

--- src/components/shared/Exercises/UnitCircleInput.tsx
+++ src/components/shared/Exercises/UnitCircleInput.tsx
@@ -1,38 +1,42 @@
 //import { useState } from 'react';
 import '../../../styles/Exercises/UnitCircleInput.scss';
 
-function UnitCircleInput (): JSX.Element {
-    return (
-        <div id="unitcircle-question-container">
-           Type the correct numbers into the blanks below!
-          <p id="unitcircle-check-question"> </p>
-          
-          <p> <i>A</i> = left  
-          <input
-            type="text"
-            className="unitcircle-check-box"
-            //onChange={handleChange}
-            //value={inputText[id]}
-          />
-          </p>
-          <p> <i>B</i> = right 
-          <input
-            type="text"
-            className="unitcircle-check-box"
-            //onChange={handleChange}
-            //value={inputText[id]}
-          />
-          </p>
-
-          <p> <i>C</i> = right 
-         <input
-            type="text"
-            className="unitcircle-check-box"
-            //onChange={handleChange}
-            //value={inputText[id]}
-          />
-          </p>
-        </div>
-      );
+function UnitCircleInput(): JSX.Element {
+  return (
+    <div id="unitcircle-question-container">
+      Type the correct numbers into the blanks below!
+      <p id="unitcircle-check-question"> </p>
+      <p>
+        {' '}
+        <i>A</i> = left
+        <input
+          type="text"
+          className="unitcircle-check-box"
+          //onChange={handleChange}
+          //value={inputText[id]}
+        />
+      </p>
+      <p>
+        {' '}
+        <i>B</i> = right
+        <input
+          type="text"
+          className="unitcircle-check-box"
+          //onChange={handleChange}
+          //value={inputText[id]}
+        />
+      </p>
+      <p>
+        {' '}
+        <i>C</i> = right
+        <input
+          type="text"
+          className="unitcircle-check-box"
+          //onChange={handleChange}
+          //value={inputText[id]}
+        />
+      </p>
+    </div>
+  );
 }
-export default UnitCircleInput
+export default UnitCircleInput;
