43f3cbd3201501c0085f5a52c2c3794652cf1e37
SanjnaT41756 <sanjnatailor@g.ucla.edu>
2024-01-30 18:40:52 -0800

    fix lint errors

--- src/components/shared/LessonSide/LessonSide.tsx
+++ src/components/shared/LessonSide/LessonSide.tsx
@@ -1,22 +1,10 @@
 import '../../../styles/LessonSide.scss';
 import penPalsLogo from '../../../assets/turtleLogo.svg';
 import Footer from './Footer';
+import * as LessonSideContent from './LessonSideContent.JSON';
 import LessonText from './LessonText';
 import LevelSelector from './LevelSelector';
 import Turtle from './Turtle';
-import * as LessonSideContent from './LessonSideContent.JSON'
-
-// const exArray: { id: string; text: string }[] = [
-//   { id: 'lesson-side-header', text: 'Positioning of Computers' },
-//   {
-//     id: 'lesson-side-body',
-//     text: 'In order to tell computers where to move the cursor, we have to use the coordinate system. The coordinate system is a grid with lots of small chunks, called units. To figure out where to move, the cursor uses its starting point at the center of the grid and counts out some number of units. For example, the point (2, 1) would be two units RIGHT and one unit UP from the center.',
-//   },
-//   {
-//     id: 'lesson-side-body',
-//     text: 'When looking at these numbers, remember that the number to move sideways comes first. A positive number means you move right, and a negative number means you move left. And when moving up and down with the second number, a positive number means you move up and a negative means down.',
-//   },
-// ];
 
 interface lessonSideProps {
   levelNum: number;
