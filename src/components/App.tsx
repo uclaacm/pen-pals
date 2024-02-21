541613e3822bdb3c4a82ddff15e0f6e7225d93e7
ShenranChen <85770861+ShenranChen@users.noreply.github.com>
2024-02-20 17:07:44 -0800

    feat: user can only click up to the max level reached in the level selector (#86)
    * limit the clickability of level selector based on the max level reached
    
    * lint fix + getting rid of console logs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CongratsPage from './shared/Congratulations';
 import ExerciseSide from './shared/ExerciseSide';
 import LessonSide from './shared/LessonSide/LessonSide';
@@ -6,10 +6,22 @@ import '../assets/WestwoodSans-Regular.ttf';
 
 function App(): JSX.Element {
   const [exerciseCount, setExerciseCount] = useState(0);
-  // const [completeExercises, setCompleteExercises] = useState(0);
+  const [completeExercises, setCompleteExercises] = useState(1);
+
+  //update complete if exercise changes
+  useEffect(() => {
+    updateComplete();
+  }, [exerciseCount]);
 
   const updateLevel = (newLevel: number): void => {
     setExerciseCount(newLevel - 1);
+  };
+
+  const updateComplete = (): void => {
+    // console.log(exerciseCount);
+    if (exerciseCount >= completeExercises) {
+      setCompleteExercises(completeExercises + 1);
+    }
   };
 
   if (exerciseCount == 5) {
@@ -22,7 +34,11 @@ function App(): JSX.Element {
   return (
     <div>
       <main>
-        <LessonSide levelNum={exerciseCount + 1} updateLevel={updateLevel} />
+        <LessonSide
+          levelNum={exerciseCount + 1}
+          updateLevel={updateLevel}
+          maxLevel={completeExercises}
+        />
         <ExerciseSide
           ExercisesNum={exerciseCount}
           incrementExercise={() => setExerciseCount(exerciseCount + 1)}
